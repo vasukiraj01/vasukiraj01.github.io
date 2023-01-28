c6f0ec3cbef143a62398cd2414388a7faa925731
vasukiraj01 <vasukigreat@gmail.com>
2023-01-28 13:24:51 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.130afacca3e5cc032e752b3be09a011d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c390b81612f9478662cd",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/css/main.bada4176.chunk.css"
+  },
+  {
+    "revision": "c390b81612f9478662cd",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/js/main.c390b816.chunk.js"
+  },
+  {
+    "revision": "4bdb9add8a2f8680fa0e",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/css/1.7b92daaf.chunk.css"
+  },
+  {
+    "revision": "4bdb9add8a2f8680fa0e",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/js/1.4bdb9add.chunk.js"
+  },
+  {
+    "revision": "8a11fba3208f98d3feb0",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/js/runtime~main.8a11fba3.js"
+  },
+  {
+    "revision": "4d77fe0d4cd4206ed15d52d002e74503",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/media/profile.4d77fe0d.png"
+  },
+  {
+    "revision": "75f85fa0704221817ee07e49ce6cde91",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/media/workbench.75f85fa0.gif"
+  },
+  {
+    "revision": "b03237e4df5bb93ec03ce3fbf72a6f42",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/media/art.b03237e4.jpg"
+  },
+  {
+    "revision": "e20657d48fa0abe2b06e7e3b603159f2",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/media/travel.e20657d4.jpg"
+  },
+  {
+    "revision": "5d963ddb0702c5fae3a60541b58a91e9",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/media/food.5d963ddb.jpg"
+  },
+  {
+    "revision": "573932f745398686dc2c1cc17b0750df",
+    "url": "/vasukiraj01/vasukiraj01.github.io/static/media/background.573932f7.png"
+  },
+  {
+    "revision": "1a8bc02748ebc7f67266c26e2a4a5c53",
+    "url": "/vasukiraj01/vasukiraj01.github.io/index.html"
+  }
+];
