d44a0f464836d1f60bedf0f4be68694e40b07509
vasukiraj01 <vasukigreat@gmail.com>
2023-02-01 17:45:55 +0530

    Updates

---
+++ precache-manifest.7666a7ff52273adc8535c0a216d1b569.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "38b83e9ea11f2688bdc0",
+    "url": "/static/css/main.89fd0d56.chunk.css"
+  },
+  {
+    "revision": "38b83e9ea11f2688bdc0",
+    "url": "/static/js/main.38b83e9e.chunk.js"
+  },
+  {
+    "revision": "4bdb9add8a2f8680fa0e",
+    "url": "/static/css/1.7b92daaf.chunk.css"
+  },
+  {
+    "revision": "4bdb9add8a2f8680fa0e",
+    "url": "/static/js/1.4bdb9add.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d77fe0d4cd4206ed15d52d002e74503",
+    "url": "/static/media/profile.4d77fe0d.png"
+  },
+  {
+    "revision": "75f85fa0704221817ee07e49ce6cde91",
+    "url": "/static/media/workbench.75f85fa0.gif"
+  },
+  {
+    "revision": "b03237e4df5bb93ec03ce3fbf72a6f42",
+    "url": "/static/media/art.b03237e4.jpg"
+  },
+  {
+    "revision": "e20657d48fa0abe2b06e7e3b603159f2",
+    "url": "/static/media/travel.e20657d4.jpg"
+  },
+  {
+    "revision": "5d963ddb0702c5fae3a60541b58a91e9",
+    "url": "/static/media/food.5d963ddb.jpg"
+  },
+  {
+    "revision": "573932f745398686dc2c1cc17b0750df",
+    "url": "/static/media/background.573932f7.png"
+  },
+  {
+    "revision": "92b61676611f6eab48a609163bb875e5",
+    "url": "/index.html"
+  }
+];
